bef60ec39929cd26090db0316470905565a9e7e1
Julia Egorova <j.e.egorova@gmail.com>
2013-05-21 16:34:48 +0400

    Method 'reset...' resets instance variables to default values

--- lib/attrio/reset.rb
+++ lib/attrio/reset.rb
@@ -2,7 +2,7 @@
 
 module Attrio
   module Reset
-    def self.included(base)    
+    def self.included(base)
       base.send(:extend, Attrio::Reset::ClassMethods)
     end
 
@@ -10,11 +10,11 @@ module Attrio
       def define_attrio_reset(as)
         define_method "reset_#{as.to_s}" do
           self.send(as.to_s).values.each do |attribute|
-            self.instance_variable_set(attribute[:instance_variable_name], nil)
+            self.instance_variable_set(attribute[:instance_variable_name], attribute[:default_value])
           end
-        end      
+        end
 
-        self.send(:alias_method, "reset_#{as.to_s}!", "reset_#{as.to_s}")  
+        self.send(:alias_method, "reset_#{as.to_s}!", "reset_#{as.to_s}")
       end
     end
   end
