a819f926ac1f9409447837a8ed0eb8d1c95f6898
Igor Alexandrov <igor.alexandrov@gmail.com>
2013-05-26 00:59:43 +0400

    Ability to reset single attribute or a set of arguments

--- lib/attrio/reset.rb
+++ lib/attrio/reset.rb
@@ -8,13 +8,14 @@ module Attrio
 
     module ClassMethods
       def define_attrio_reset(as)
-        define_method "reset_#{as.to_s}" do
+        define_method "reset_#{as.to_s}" do |attributes_to_reset = []|
+          attributes_to_reset = Array.wrap(attributes_to_reset).flatten          
+          attributes_to_reset = attributes_to_reset.empty? ? self.send(as.to_s).keys : self.send(as.to_s).keys & attributes_to_reset.map!(&:to_sym)
 
-          # self.send(as.to_s).values.each { |attribute| attribute.reset! }
-          self.send(as.to_s).values.each do |attribute|                        
+          attributes_to_reset.each do |attribute_name|
+            attribute = self.send(as.to_s)[attribute_name]
             self.send(attribute.writer_method_name, attribute.default_value)            
           end
-
         end
 
         self.send(:alias_method, "reset_#{as.to_s}!", "reset_#{as.to_s}")        
