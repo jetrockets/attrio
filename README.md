22b0da462e76c7de618d43569abbac47e51ff8e4
Igor Alexandrov <igor.alexandrov@gmail.com>
2013-05-27 11:28:37 +0400

    Added embed value spec and example

--- README.md
+++ README.md
@@ -118,7 +118,61 @@ class Page
     end    
   end
 end
+```
 
+### Embed Value
+You can embed values in Attrio just like you do it in Virtus.
+
+```ruby
+module MassAssignment
+  def initialize(attributes = {})
+  self.attributes = attributes
+  end
+
+  def attributes=(attributes = {})
+  attributes.each do |attr,value|
+      self.send("#{attr}=", value) if self.respond_to?("#{attr}=")
+  end
+  end
+end
+
+class City  
+  include Attrio
+  include MassAssignment
+
+  define_attributes do
+  attr :name, String
+  end
+end
+
+class Address
+  include Attrio
+  include MassAssignment
+
+  define_attributes do
+  attr :street,  String
+  attr :zipcode, String
+  attr :city,    City
+  end
+end
+
+class User
+  include Attrio
+  include MassAssignment
+
+  define_attributes do
+  attr :name,    String
+  attr :address, Address
+  end
+end
+
+user = User.new( :address => { :street => 'Sklizkova 6A', :zipcode => '170000', :city => { :name => 'Tver' } } )
+user.address.street
+# => 'Sklizkova 6A'
+user.address.zipcode
+# => '170000'
+user.address.city.name
+# => 'Tver'
 ```
 
 ### Methods visibility
---
+++ spec/attrio/embed_value_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe 'Embedded Value' do
+  before do
+    module MassAssignment
+      def initialize(attributes = {})
+        self.attributes = attributes
+      end
+
+      def attributes=(attributes = {})
+        attributes.each do |attr,value|
+          self.send("#{attr}=", value) if self.respond_to?("#{attr}=")
+        end
+      end
+    end
+
+    class City  
+      include Attrio
+      include MassAssignment
+
+      define_attributes do
+        attr :name, String
+      end
+    end
+
+    class Address
+      include Attrio
+      include MassAssignment
+
+      define_attributes do
+        attr :street,  String
+        attr :zipcode, String
+        attr :city,    City
+      end
+    end
+
+    class User
+      include Attrio
+      include MassAssignment
+
+      define_attributes do
+        attr :name,    String
+        attr :address, Address
+      end
+    end
+  end
+
+  let(:address_attributes) do
+      { :street => 'Sklizkova 6A', :zipcode => '170000', :city => { :name => 'Tver' } }
+  end
+
+  it 'should allow to pass a hash for the embedded value' do
+    user = User.new
+    user.address = address_attributes
+    user.address.street.should == 'Sklizkova 6A'
+    user.address.zipcode.should == '170000'
+    user.address.city.name.should == 'Tver'
+  end
+end
