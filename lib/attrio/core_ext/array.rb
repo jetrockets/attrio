32a94df0dff6e089d56389ec50500d32d0ed8e1f
Josh Cheek <josh.cheek@gmail.com>
2013-05-31 01:04:59 -0500

    Remove Hash#extractable_options core extension

--- lib/attrio/core_ext/array.rb
+++ lib/attrio/core_ext/array.rb
@@ -1,14 +1,6 @@
 # encoding: utf-8
 
 class Array # :nodoc:
-  def extract_options!
-    if last.is_a?(Hash) && last.extractable_options?
-      pop
-    else
-      {}
-    end
-  end unless method_defined?(:extract_options!)
-
   class << self
     def wrap(object)
       if object.nil?
